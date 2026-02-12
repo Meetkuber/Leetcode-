919fc291a6a9cdd327fd1a3bcd8ba66801243b93
Meet Kuber <meetkuber30@gmail.com>
2026-02-12 23:12:24 +0530

    Time: 484 ms (81.58%) | Memory: 19.7 MB (60.35%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,21 +1,21 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         coins.sort()
-        memo = {0:0}
-
-        def min_coins(amt):
-            if amt in memo:
-                return memo[amt]
+        dp = [0]* ( amount + 1)
+        
+        for i in range(1, amount+1):
             minn = float('inf')
+
             for coin in coins:
-                diff = amt - coin
+                diff = i - coin
                 if diff < 0:
                     break
-                minn = min(minn , 1 + min_coins(diff))
-            memo[amt] = minn
-            return minn
-        result = min_coins(amount)
-        if result < float('inf'):
-            return result
+                minn = min(minn , dp[diff] + 1)
+
+            dp[i] = minn
+        if dp[amount] < float('inf'):
+            return dp[amount]
         else:
             return -1
+
+        
